02f7040a30ae039d7797fb8ed2ef3b87b94f94b1
ridalo <ridalo1989@gmail.com>
2022-10-30 22:55:11 -0400

    window alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("This is an alert! JavaScript is running!");
+ var playerName = window.prompt("What is your robot's name?");
+    window.alert(playerName);
+    function fight() {
+        window.alert("The fight has begun!");
+      }
+
+      // fight(); 
