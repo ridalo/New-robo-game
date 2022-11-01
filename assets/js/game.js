a2440498aba592a055a756ed2ce25346c208dc0a
ridalo <ridalo1989@gmail.com>
2022-10-31 22:18:51 -0400

    bullshit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,8 @@
  var playerName = window.prompt("What is your robot's name?");
-    window.alert(playerName);
+    console.log(playerName);
+    console.log("This logs a string, good for leaving yourself a message");
+    console.log(10 + 10);
+    console.log("Our robot's name is " + playerName);
     function fight() {
         window.alert("The fight has begun!");
       }
