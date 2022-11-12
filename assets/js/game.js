9d174d7179f2f7cbaad7f26f681c2bc7f084828b
ridalo <ridalo1989@gmail.com>
2022-11-11 22:33:14 -0500

    made operations and scopes outside the function.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,35 @@
- var playerName = window.prompt("What is your robot's name?");
-    console.log(playerName);
-    console.log("This logs a string, good for leaving yourself a message");
-    console.log(10 + 10);
-    console.log("Our robot's name is " + playerName);
-    function fight() {
-        window.alert("The fight has begun!");
-      }
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-      // fight(); 
+// logging multiple values at once
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  // Alert player that round is starting
+  window.alert("welcome to Robot Gladiators!")
+
+
+//subtract 'playerAttack' value from 'enemyHealth' value and use result.
+enemyHealth = enemyHealth - playerAttack;
+//log result to console so we know it happened.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+//subtract 'enenemyAttack' value from 'playerHealth' and use result.
+
+//log result to console.
+playerHealth = playerHealth - enemyAttack;
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+};
+// function declaration is when you create a function using the key word first.
+
+
+fight();
+
